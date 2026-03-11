6b0513ef73915991900cd95e1f8c3cf0ab4ab65f
Tharun PK <draptorcreations@gmail.com>
2026-03-11 12:44:55 +0530

    Add files via upload

---
+++ todo_list.py
@@ -0,0 +1,25 @@
+print("Welcome to your TO-DO List")
+count = False
+chance=0
+task= 2
+
+
+a = input("Mention your Tasks:")
+b = input("want to add more?[y/n]")
+
+while chance < task:
+    if b == 'y':
+        c = input("add your task:")
+        d = input("want to add more?[y/n]")
+    
+    elif d=='y':
+        e = input("add your task:")
+
+    elif d=='n':
+        print("your table")
+        print(a, endl=" ")
+        print(c, endl=" ")
+    
+
+
+    
